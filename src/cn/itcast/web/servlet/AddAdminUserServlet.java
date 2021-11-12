0d75b4de8765877eb2352a71aa587ac154389b2f
hj <2207392187@qq.com>
2021-11-12 09:19:23 +0800

    整合

---
+++ src/cn/itcast/web/servlet/AddAdminUserServlet.java
@@ -0,0 +1,56 @@
+package cn.itcast.web.servlet;
+
+import cn.itcast.dao.impl.UserDaoImpl;
+import cn.itcast.domain.User;
+import cn.itcast.service.UserService;
+import cn.itcast.service.impl.UserServiceImpl;
+import org.apache.commons.beanutils.BeanUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
+@WebServlet("/addAdminUserServlet")
+public class AddAdminUserServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //1.设置编码
+        request.setCharacterEncoding("utf-8");
+        //2.获取参数
+        Map<String, String[]> map = request.getParameterMap();
+        //3.封装对象
+        User user = new User();
+        try {
+            BeanUtils.populate(user,map);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        //4.调用Service保存
+        UserService service = new UserServiceImpl();
+
+        UserDaoImpl userDao = new UserDaoImpl();
+        if(null==userDao.findUserByUsername(user.getName())){
+            service.addAdminUser(user);
+        }else{
+
+            //提示信息
+            request.setAttribute("login_msg","用户已存在！");
+            request.getRequestDispatcher("/addAdmin.jsp").forward(request,response);
+        }
+        //5.跳转到userListServlet
+        response.sendRedirect(request.getContextPath()+"/loginServlet");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+}
